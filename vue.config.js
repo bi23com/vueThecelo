079e919d5f364698e54ff59e4e8a24875400476a
bi23 <support@bi23.com>
2022-01-17 13:52:20 +0800

    Add files via upload

---
+++ vue.config.js
@@ -0,0 +1,141 @@
+// vue.config.js 配置说明
+// 这里只列一部分，具体配置惨考文档啊
+const config = require('./package.json');
+const productionGzipExtensions = ['html', 'js', 'css'];
+const CompressionWebpackPlugin = require('compression-webpack-plugin');  // 实现Gzip压缩
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');   // 进行js文件的压缩。
+const isProduction = process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'development';
+// const isProduction = false;
+console.log(isProduction+"===isProduction")
+const ip = require('ip');
+const myIp = ip.address();
+module.exports = {
+  // 将部署应用程序的基本URL
+  // 将部署应用程序的基本URL。
+  // 默认情况下，Vue CLI假设您的应用程序将部署在域的根目录下。
+  // https://www.my-app.com/。如果应用程序部署在子路径上，则需要使用此选项指定子路径。例如，如果您的应用程序部署在https://www.foobar.com/my-app/，集baseUrl到'/my-app/'.
+
+  publicPath: process.env.APP_CHUNK || '/',
+  // build时构建文件的目录 构建时传入 --no-clean 可关闭该行为
+  // build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  outputDir: "dist",
+  // build时放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录
+  assetsDir: 'staticNew', // 配置js、css静态资源二级目录的位置
+
+  /*
+    构建多页面模式的应用程序.每个“页面”都应该有一个相应的JavaScript条目文件。该值应该是一
+    个对象，其中键是条目的名称，而该值要么是指定其条目、模板和文件名的对象，要么是指定其条目
+    的字符串，
+    注意：请保证pages里配置的路径和文件名 在你的文档目录都存在 否则启动服务会报错的
+  */
+  // pages:{ type:Object,Default:undfind }
+  // pages: {
+  // index: {
+  // entry for the page
+  // entry: 'src/index/main.js',
+  // the source template
+  // template: 'public/index.html',
+  // output as dist/index.html
+  // filename: 'index.html'
+  // },
+  // when using the entry-only string format,
+  // template is inferred to be `public/subpage.html`
+  // and falls back to `public/index.html` if not found.
+  // Output filename is inferred to be `subpage.html`.
+  // subpage: 'src/subpage/main.js'
+  // },
+  //   lintOnSave：{ type:Boolean default:true } 问你是否使用eslint
+  lintOnSave: false,
+  chainWebpack: config => {
+    // ============压缩图片 start============
+    config.module
+      .rule('images')
+      .use('image-webpack-loader')
+      .loader('image-webpack-loader')
+      .options({ bypassOnDebug: true })
+      .end()
+    // ============压缩图片 end============
+  },
+  configureWebpack: config => {
+    if (isProduction) {
+      // 代码压缩
+      config.plugins.push(
+        new UglifyJsPlugin({
+          uglifyOptions: {
+            warnings: false,
+            compress: {
+              drop_debugger: true,
+              drop_console: true,
+              pure_funcs: ['console.log']
+            },
+          },
+          sourceMap: false,
+          parallel: true,
+        })
+      )
+      // gzip压缩
+      config.plugins.push(
+        new CompressionWebpackPlugin({
+          filename: '[path].gz[query]',
+          algorithm: 'gzip',
+          test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'
+          ),
+          threshold: 10240, // 只有大小大于该值的资源会被处理 10240
+          minRatio: 0.8, // 只有压缩率小于这个值的资源才会被处理
+          deleteOriginalAssets: false // 删除原文件
+        })
+      )
+      // 公共代码抽离
+      config.optimization = {
+        splitChunks: {
+          cacheGroups: {
+            vendor: {
+              chunks: 'all',
+              test: /node_modules/,
+              name: 'vendor',
+              minChunks: 1,
+              maxInitialRequests: 5,
+              minSize: 0,
+              priority: 100
+            },
+            common: {
+              chunks: 'all',
+              test: /[\\/]src[\\/]js[\\/]/,
+              name: 'common',
+              minChunks: 2,
+              maxInitialRequests: 5,
+              minSize: 0,
+              priority: 60
+            },
+            styles: {
+              name: 'styles',
+              test: /\.(sa|sc|c)ss$/,
+              chunks: 'all',
+              enforce: true
+            },
+            runtimeChunk: {
+              name: 'manifest'
+            }
+          }
+        }
+      }
+    }
+  },
+  //默认情况下 babel-loader 会忽略所有 node_modules 中的文件。如果你想要通过 Babel 显式转译一个依赖，可以在这个选项中列出来。
+  // transpileDependencies: ['qrcodejs2'],
+  // productionSourceMap：{ type:Bollean,default:true } 生产源映射
+  // 如果您不需要生产时的源映射，那么将此设置为false可以加速生产构建
+  productionSourceMap: false,
+  // devServer:{type:Object} 3个属性host,port,https
+  // 它支持webPack-dev-server的所有选项
+
+  devServer: {
+    port: 8080, // 端口号
+    host: myIp,
+    https: false, // https:{type:Boolean}
+    open: true, //配置自动启动浏览器
+    // proxy: 'http://121.40.172.190:7000', // 配置跨域处理,只有一个代理
+    proxy: 'https://thecelo.com/', // 配置跨域处理,只有一个代理
+    // proxy: 'https://alfajores-forno.celo-testnet.org', // 配置跨域处理,只有一个代理
+  },
+};
